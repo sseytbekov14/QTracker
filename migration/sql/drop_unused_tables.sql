90bc663f8a75b7eef52118b44a61e1de79117076
Seytbekov <sseytbekov@kpmg.kz>
2026-02-24 14:03:02 +0500

    Remove migration folder and add to gitignore

--- migration/sql/drop_unused_tables.sql
+++
@@ -1,7 +0,0 @@
--- Drop unused tables: notification_reads and workflow_comments
--- These tables are not used by the application.
--- notification_reads: read status is tracked via notifications.is_read column
--- workflow_comments: comments are stored in workflow_history.comments column
-
-DROP TABLE IF EXISTS notification_reads;
-DROP TABLE IF EXISTS workflow_comments;
